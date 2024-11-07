dbef3feeae2cd4e3368ba8df5413f76e26d96ada
Lowiizz <153203122+TranBaLoi@users.noreply.github.com>
2024-11-07 14:52:50 +0700

    finish

--- app_core/models.py
+++ app_core/models.py
@@ -23,7 +23,7 @@ class Deposit(models.Model):
     student = models.ForeignKey(Student_Account, on_delete = models.CASCADE)
     full_name = models.CharField(max_length = 200)
     card_number = models.CharField(max_length=20)
-    bank = models.CharField(max_length = 6)
+    bank = models.CharField(max_length = 10)
     date_submit = models.CharField(max_length = 100)
     amount = models.IntegerField()
     confirm = models.BooleanField(default=False)
@@ -65,12 +65,13 @@ class Block(models.Model):
 
 
 class Rooms(models.Model):
-    block = models.ForeignKey(Block, on_delete = models.CASCADE)
-    room_name = models.CharField(max_length = 50, null=True)
-    room_tag = models.CharField(max_length = 10, default="NEW")
+    block = models.ForeignKey(Block, on_delete=models.CASCADE)
+    room_name = models.CharField(max_length=50, null=True)
+    room_tag = models.CharField(max_length=10, default="NEW")
     description = models.TextField()
     condition = models.TextField()
     charge = models.IntegerField()
+    slot_available = models.IntegerField(default=2) # tối đa 10 giường 
     is_available = models.BooleanField(default=True)
     image1 = models.ImageField(upload_to="photos")
     image2 = models.ImageField(upload_to="photos")
@@ -86,7 +87,6 @@ class Rooms(models.Model):
     admin_photo.allow_tag = True
     def __str__(self):
         return self.room_name
-
 
 class Accomodation_Request(models.Model):
     student = models.OneToOneField(Student_Account, on_delete = models.CASCADE)
@@ -132,13 +132,35 @@ class GuestStayRequest(models.Model):
 class RoomTransfer(models.Model):
     student = models.ForeignKey(Student_Account, on_delete=models.CASCADE)
     reason = models.TextField()
-    room = models.ForeignKey(Rooms, on_delete=models.CASCADE)
+    room = models.ForeignKey(Rooms, on_delete=models.CASCADE, related_name='new_room')  # phòng chuyển đến
     approved = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now = True)
-    updated_at = models.DateTimeField(auto_now_add = True)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    # def save(self, *args, **kwargs):
+    #     # Lấy thông tin phòng hiện tại của sinh viên trước khi cập nhật
+    #     accommodation_request = Accomodation_Request.objects.get(student=self.student)
+    #     current_room = accommodation_request.room
+
+        # # Nếu việc chuyển phòng đã được duyệt và phòng mới khác phòng hiện tại
+        # if self.approved and current_room != self.room:
+        #     # Giảm số người ở phòng cũ
+        #     current_room.cur_people = max(current_room.cur_people - 1, 0)
+        #     current_room.save()
+
+        #     # Tăng số người ở phòng mới
+        #     self.room.cur_people = min(self.room.cur_people + 1, self.room.sum_people)
+        #     self.room.save()
+
+        #     # Cập nhật phòng mới cho student trong Accomodation_Request
+        #     accommodation_request.room = self.room
+        #     accommodation_request.save()
+
+        # super(RoomTransfer, self).save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.student.first_name} - {self.room.room_name}"
+
 
 
 
